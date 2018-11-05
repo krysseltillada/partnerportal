08831a32bd7f8ceb0a4dd3afe43e992a9488da66
krysseltillada <krysseltillada@gmail.com>
2018-11-06 01:21:54 +0800

    putted the stack template, created a structure for templates and resources files get some sample index page to load

--- src/main/java/com/codebytes/autobook/autobook/AutobookApplication.java
+++ src/main/java/com/codebytes/autobook/autobook/AutobookApplication.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AutobookApplication {
 
-	@GetMapping("/")
+	@GetMapping("/admin")
 	public String home () {
-		return "home";
+		return "/admin/index";
 	}
 
 	public static void main(String[] args) {
